3ebdd93bb0ede06852ef19ed90e4b0961bcdf0e8
cbot918 <cbot918@gmail.com>
2023-01-13 03:30:25 +0800

    update-home-title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Yale Blog',
+  tagline: 'Coding is Fun',
+  url: 'https://github.com/cbot918',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Yale Blog',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
